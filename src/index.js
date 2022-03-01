86d7c15f6eea22c1675e7ac18eb38d66bb8cd7ec
Dallowin <adil.2@yandex.ru>
2022-03-02 00:41:18 +0300

    remove plugin

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-
 createApp(App)
     .use(router)
     .use(store)
-    .use(VueAxios, axios)
     .mount('#root')
